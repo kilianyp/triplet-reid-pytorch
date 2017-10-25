Fix BatchHard implementation: - Derivative of Squarerroot(0) is undefined. - Make independent of Batch size and targets. - Wrong calculation of furthest_negative.

tests/test_losses.py
@@ -5,8 +5,19 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 import triplet_loss as l
 import unittest
 import numpy as np
+import scipy.spatial as sp
 import torch
 from torch.autograd import Variable
+
+
+class TestCdist(unittest.TestCase):
+    def test_cdist_euclidean(self):
+        a = np.random.rand(10, 20)
+        b = np.random.rand(30, 20)
+        metric = "euclidean"
+        D_torch = l.calc_cdist(torch.from_numpy(a), torch.from_numpy(b), metric)
+        D_sp = sp.distance.cdist(a, b, metric)
+        np.testing.assert_array_almost_equal(D_torch.numpy(), D_sp)
 
 class TestBatchHard(unittest.TestCase):
     def test_batch_hard(self):
@@ -25,8 +36,8 @@ class TestBatchHard(unittest.TestCase):
         loss_fn = l.BatchHard("none")
         loss = loss_fn(features, pids)
         
-        result = np.array([2.0 - 4.0, 1.0 - 3.5, 8.5 - 4.0, 2.0 - 2.5, 8.5 - 2.5])
-        np.testing.assert_array_equal(result, loss.data)
+        result = np.array([2.0 - 4.0, 1.0 - 3.5, 8.5 - 4.0, 2.0 - 2.5, 8.5 - 2.5], dtype=np.float32)
+        np.testing.assert_array_equal(result, loss.data.numpy())
 
 
 if __name__ == '__main__':

triplet_loss.py
@@ -5,8 +5,9 @@ import torch
 
 def calc_cdist(a, b, metric='euclidean'):
     if metric == 'euclidean':
-        diff = a[None, :, :] - b[:, None, :]
-        return torch.sqrt(torch.sum(torch.mul(diff, diff), dim=2))
+        diff = a[:, None, :] - b[None, :, :]
+        # derivative of sqrt(0) is not defined!
+        return torch.sqrt(torch.sum(torch.mul(diff, diff), dim=2) + 1e-12)
     else:
         raise NotImplementedError("Metric %s has not been implemented!" % metric)
 
@@ -16,6 +17,7 @@ class BatchHard(nn.Module):
     def __init__(self, m):
         super(BatchHard, self).__init__()
         self.m = m
+        self.name = "BatchHard"
 
     def forward(self, embd, pids):
         """Caluclates the batch hard loss as in in arxiv.org/abs/1703.07737.
@@ -24,28 +26,15 @@ class BatchHard(nn.Module):
             embd: 2D tensor of the embedding vector shape [batch_size, Features]
             pids: 1D tensor of the identities of shape [batch_size]. Tensor has to be of 
         """
-
         cdist = calc_cdist(embd, embd)
         mask_max = pids[None, :] == pids[:, None]
 
         mask_min = 1 - mask_max.data
-
-        #furthest_positive = torch.max(
-        #        torch.mask_select(cdist, mask_max).view(self.K, self.batch_size),
-        #        dim=0)[0]  # max returns tuple, also indexes
-        #closest_negative = torch.min(
-        #        torch.mask_select(cdist, mask_min).view(batch_size - K,batch_size),
-        #        dim=0)[0]
         furthest_positive = torch.max(cdist * mask_max.float(), dim=0)[0]
-        furthest_positive -= torch.mean(furthest_positive)
+        furthest_negative = [torch.min(cdists[mask_min[id]]) for id, cdists in enumerate(cdist)]
+        furthest_negative = torch.stack(furthest_negative).squeeze() # stacking adds another dimension
         
-        furthest_negative = [torch.max(cdists[mask_min[id]])[0] for id, cdists in enumerate(cdist)]
-        furthest_negative = torch.stack(furthest_negative)
-        print(furthest_negative)
-        furthest_negative = torch.min(furthest_negative, dim=0)[0]
-
         diff = furthest_positive - furthest_negative
-
         if isinstance(self.m, float):
             diff = (diff + self.m).clamp(min=0)
         elif self.m.lower() == "soft":