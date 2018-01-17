e7936303465b736c1fa82ef59a8dfbfad3d8fc49
Kilian Pfeiffer <kilian.pfeiffer@rwth-aachen.de>
2018-01-17 16:18:08 +0100

    Make embed.py functions reusable.

--- embed.py
+++ embed.py
@@ -59,7 +59,7 @@ transforms = transforms.Compose([
 def clean_dict(dic):
     """Removes module from keys. For some reason those are added when saving."""
     fresh_dict = {}
-    for key, value in state_dict.items():
+    for key, value in dic.items():
         prefix = "module."
         if key.startswith(prefix):
             key = key[len(prefix):]
@@ -74,65 +74,66 @@ def extract_csv_name(csv_file):
     else:
         return filename
 
-args = parser.parse_args()
+if __name__ == "__main__":
+    args = parser.parse_args()
 
-csv_file = os.path.expanduser(args.csv_file)
-data_dir = os.path.expanduser(args.data_dir)
-model_dir = os.path.expanduser(args.model)
+    csv_file = os.path.expanduser(args.csv_file)
+    data_dir = os.path.expanduser(args.data_dir)
+    model_dir = os.path.expanduser(args.model)
 
-model_path = os.path.realpath(args.model).split('/')[-2]
-if args.filename == None:
-    model_name = os.path.basename(args.model)
-    csv_name = extract_csv_name(csv_file)
-    output_file = "%s_%s_embeddings.h5" % (csv_name, model_name)
-else:
-    output_file = args.filename
+    model_path = os.path.realpath(args.model).split('/')[-2]
+    if args.filename == None:
+        model_name = os.path.basename(args.model)
+        csv_name = extract_csv_name(csv_file)
+        output_file = "%s_%s_embeddings.h5" % (csv_name, model_name)
+    else:
+        output_file = args.filename
 
-if not os.path.isdir(args.output_dir):
-    os.mkdir(args.output_dir)
-output_dir = os.path.join(args.output_dir, model_path)
-if not os.path.isdir(output_dir):
-    os.mkdir(output_dir)
+    if not os.path.isdir(args.output_dir):
+        os.mkdir(args.output_dir)
+    output_dir = os.path.join(args.output_dir, model_path)
+    if not os.path.isdir(output_dir):
+        os.mkdir(output_dir)
 
 
-output_file = os.path.join(os.path.abspath(output_dir), output_file)
-print(output_file)
+    output_file = os.path.join(os.path.abspath(output_dir), output_file)
+    print(output_file)
 
-if os.path.isfile(output_file):
-    #TODO create numerated filename
+    if os.path.isfile(output_file):
+        #TODO create numerated filename
+        print(output_file, file=sys.stderr)
+        print("File %s already exists! Please choose a different name." % output_file)
+        sys.exit(0)
+    else:
+        print("Creating file in %s" % output_file)
+
+    dataset = CsvDataset(csv_file, data_dir, transform=transforms)
+
+    dataloader = torch.utils.data.DataLoader(
+                dataset,
+                batch_size
+            )
+
+    model = trinet()
+    #restore trained model
+    state_dict = torch.load(model_dir)
+    state_dict = clean_dict(state_dict)
+    model.load_state_dict(state_dict)
+    model = torch.nn.DataParallel(model).cuda()
+    model.eval()
+
+
+    import gc
+    with h5py.File(output_file) as f_out:
+        emb_dataset = f_out.create_dataset('emb', shape=(len(dataset), embedding_dim), dtype=np.float32)
+        start_idx = 0
+        for idx, (data, _, _) in enumerate(dataloader):
+            data = data.cuda()
+            data = Variable(data)
+            result = model(data)
+            end_idx = start_idx + len(result)
+            emb_dataset[start_idx:end_idx] = result.data.cpu().numpy()
+            start_idx = end_idx
+            print("Done (%d/%d)" % (idx, len(dataloader)))
+            gc.collect()
     print(output_file, file=sys.stderr)
-    print("File %s already exists! Please choose a different name." % output_file)
-    sys.exit(0)
-else:
-    print("Creating file in %s" % output_file)
-
-dataset = CsvDataset(csv_file, data_dir, transform=transforms)
-
-dataloader = torch.utils.data.DataLoader(
-            dataset,
-            batch_size
-        )
-
-model = trinet()
-#restore trained model
-state_dict = torch.load(model_dir)
-state_dict = clean_dict(state_dict)
-model.load_state_dict(state_dict)
-model = torch.nn.DataParallel(model).cuda()
-model.eval()
-
-
-import gc
-with h5py.File(output_file) as f_out:
-    emb_dataset = f_out.create_dataset('emb', shape=(len(dataset), embedding_dim), dtype=np.float32)
-    start_idx = 0
-    for idx, (data, _, _) in enumerate(dataloader):
-        data = data.cuda()
-        data = Variable(data)
-        result = model(data)
-        end_idx = start_idx + len(result)
-        emb_dataset[start_idx:end_idx] = result.data.cpu().numpy()
-        start_idx = end_idx
-        print("Done (%d/%d)" % (idx, len(dataloader)))
-        gc.collect()
-print(output_file, file=sys.stderr)
