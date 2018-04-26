e7882298d889cf4edb3b22eb03a0483fda07a761
Kilian Pfeiffer <kilian.pfeiffer@rwth-aachen.de>
2018-04-26 17:42:00 +0200

    Unify embedding.

--- embed.py
+++ embed.py
@@ -10,39 +10,10 @@ from trinet import trinet
 import os
 import h5py
 import sys
-
 from argparse import ArgumentParser
+import json
 
 
-parser = ArgumentParser()
-
-parser.add_argument(
-        '--output_dir', default="embed",
-        help="Output directory for embedding hd5 file."
-        )
-parser.add_argument(
-        '--filename', default=None, 
-        help="Output filename")
-
-parser.add_argument(
-        '--csv_file', required=True,
-        help="CSV file containing relative paths.")
-
-parser.add_argument(
-        '--data_dir', required=True,
-        help="Root dir where the data is stored. This and the paths in the\
-        csv file have to result in the correct file path."
-        )
-
-parser.add_argument(
-        '--model', required=True,
-        help="Path to state dict of model."
-        )
-
-parser.add_argument(
-        '--n_latent', required=True, type=int,
-        help="Dimension of latent variable."
-        )
 normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                 std=[0.229, 0.224, 0.225])
 
@@ -83,10 +54,12 @@ def extract_csv_name(csv_file):
     else:
         return filename
 
-def create_embeddings(csv_file, data_dir, model_dir, filename=None, output_dir="embed"):
-    model_path = os.path.realpath(model_dir).split('/')[-2]
+
+def write_to_h5(csv_file, data_dir, model_file, filename=None, output_dir="embed"): 
+
+    experiment = os.path.realpath(model_file).split('/')[-2]
     if filename == None:
-        model_name = os.path.basename(model_dir)
+        model_name = os.path.basename(model_file)
         csv_name = extract_csv_name(csv_file)
         output_file = "%s_%s.h5" % (csv_name, model_name)
     else:
@@ -94,7 +67,7 @@ def create_embeddings(csv_file, data_dir, model_dir, filename=None, output_dir="
 
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
-    output_dir = os.path.join(output_dir, model_path)
+    output_dir = os.path.join(output_dir, experiment)
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
 
@@ -115,40 +88,117 @@ def create_embeddings(csv_file, data_dir, model_dir, filename=None, output_dir="
                 dataset,
                 batch_size
             )
+    with h5py.File(output_file) as f_out:
+        emb_dataset = f_out.create_dataset('emb', shape=(len(dataset), 128), dtype=np.float32)
+        start_idx = 0
+        for result in create_embeddings(dataloader, model_file):
+            end_idx = start_idx + len(result)
+            emb_dataset[start_idx:end_idx] = result
+            start_idx = end_idx
+    return output_file
 
-    model = mgn(dim=128, num_classes=751)
+def get_args_path(model_path):
+    model_path = os.path.dirname(model_path)
+    return os.path.join(model_path, "args.json")
+
+
+class InferenceModel(object):
+    def __init__(self, model_path, transform=transform_comp, cuda=True):
+        self.cuda = cuda
+
+        self.transform = transform
+        model = restore_model(model_path)
+        if self.cuda:
+            model = model.cuda()
+        
+        model.eval()
+        self.model = model
+        self.endpoints = {}
+
+    def __call__(self, data):
+        """Forward pass on an image.
+        Args:
+            data: A PIL image
+        """
+
+        data = Variable(self.transform(data))
+        print(data.size())
+        if self.cuda:
+            data = data.cuda()
+        self.endpoints = self.model(data, self.endpoints)
+        result = self.endpoints["emb"]
+        # mean over crops
+        # TODO
+        result = result.mean(0)
+        return result
+
+def restore_model(model_path):
+    args_path = get_args_path(model_path)
+
+    with open(args_path, 'r') as f_handle:
+        args = json.load(f_handle)
+    
+    model_parameters = {"dim": args["embedding_dim"], "num_classes": args["num_classes"]}
+    model_module = __import__('trinet')
+    model = getattr(model_module, args["model"])
+    model = model(**model_parameters)
     #restore trained model
-    state_dict = torch.load(model_dir)
+    state_dict = torch.load(model_path)
     state_dict = clean_dict(state_dict)
     model.load_state_dict(state_dict)
+    return model
+
+def create_embeddings(dataloader, model_path):
+    """Create ten fold embeddings."""
+    
+    model = restore_model(model_path)
     model = torch.nn.DataParallel(model).cuda()
     model.eval()
 
     endpoints = {}
-    import gc
-    with h5py.File(output_file) as f_out:
-        emb_dataset = f_out.create_dataset('emb', shape=(len(dataset), 128), dtype=np.float32)
-        start_idx = 0
-        for idx, (data, _, _) in enumerate(dataloader):
-            data = Variable(data)
-            # with cropping there is an additional dimension
-            bs, ncrops, c, h, w = data.size()
-            endpoints = model(data.view(-1, c, h, w), endpoints)
-            result = endpoints["emb"]
-            #restore batch and crops dimension and use mean over all crops
-            result = result.view(bs, ncrops, -1).mean(1)
-            end_idx = start_idx + len(result)
-            emb_dataset[start_idx:end_idx] = result.data.cpu().numpy()
-            start_idx = end_idx
-            print("\rDone (%d/%d)" % (idx, len(dataloader)), flush=True, end='')
-            gc.collect()
-    print()
-    return output_file
+    # this is important, otherwise there might be a race condition
+    # which gpu sets emb first => will lead to batch contains only values from one gpu twice
+    endpoints["emb"] = None
+    for idx, (data, _, path) in enumerate(dataloader):
+        data = Variable(data).cuda()
+        # with cropping there is an additional dimension
+        bs, ncrops, c, h, w = data.size()
+        endpoints = model(data.view(-1, c, h, w), endpoints)
+        result = endpoints["emb"]
+        #restore batch and crops dimension and use mean over all crops
+        result = result.view(bs, ncrops, -1).mean(1)
+        print("\rDone (%d/%d)" % (idx, len(dataloader)), flush=True, end='')
+        yield  result.data.cpu().numpy()
 
 if __name__ == "__main__":
+    parser = ArgumentParser()
+
+    parser.add_argument(
+            '--output_dir', default="embed",
+            help="Output directory for embedding hd5 file."
+            )
+    parser.add_argument(
+            '--filename', default=None, 
+            help="Output filename")
+
+    parser.add_argument(
+            '--csv_file', required=True,
+            help="CSV file containing relative paths.")
+
+    parser.add_argument(
+            '--data_dir', required=True,
+            help="Root dir where the data is stored. This and the paths in the\
+            csv file have to result in the correct file path."
+            )
+
+    parser.add_argument(
+            '--model', required=True,
+            help="Path to state dict of model."
+            )
     args = parser.parse_args()
 
     csv_file = os.path.expanduser(args.csv_file)
     data_dir = os.path.expanduser(args.data_dir)
     model_dir = os.path.expanduser(args.model)
+    write_to_h5(csv_file, data_dir, model_dir, args.filename, args.output_dir)
 
--- evaluate.py
+++ evaluate.py
@@ -2,7 +2,7 @@ import argparse
 import subprocess
 import sys
 import os
-from embed import create_embeddings
+from embed import write_to_h5
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", help="Which dataset to evaluate", required=True)
@@ -10,6 +10,7 @@ parser.add_argument("--model", help="Path to model", required=True)
 parser.add_argument("--query", help="Path to query csv", required=True)
 parser.add_argument("--gallery", help="Path to gallery csv", required=True)
 parser.add_argument("--data_dir", help="Path to image directory", required=True)
+parser.add_argument("--batch_size", help="Batch size", type=int, default=32)
 parser.add_argument("--prefix", required=False)
 args = parser.parse_args()
 model = args.model
@@ -22,13 +23,13 @@ query_csv = os.path.expanduser(args.query)
 data_dir = os.path.expanduser(args.data_dir)
 
 
-gallery_embeddings = create_embeddings(gallery_csv, data_dir, model)
+gallery_embeddings = write_to_h5(gallery_csv, data_dir, model)
 # generated filename is written in stderr, remove some whitecharacters.
 
 if gallery_csv == query_csv:
     query_embeddings = gallery_embeddings
 else:
-    query_embeddings = create_embeddings(query_csv, data_dir, model)
+    query_embeddings = write_to_h5(query_csv, data_dir, model)
 
 print("Evaluating query: {}, gallery {}".format(query_csv, gallery_csv))
 eval_args = ["python3", "/home/pfeiffer/Projects/cupsizes/evaluate.py",
@@ -38,7 +39,7 @@ eval_args = ["python3", "/home/pfeiffer/Projects/cupsizes/evaluate.py",
              "--gallery_dataset", gallery_csv,
              "--gallery_embeddings", gallery_embeddings,
              "--metric", "euclidean",
-             "--batch_size", "32"]
+             "--batch_size", args.batch_size]
 
 
 file_name =  "{}{}_{}_{}.txt".format(args.prefix, os.path.basename(query_csv), os.path.basename(gallery_csv), os.path.basename(model))
@@ -46,6 +47,7 @@ file_name =  "{}{}_{}_{}.txt".format(args.prefix, os.path.basename(query_csv), o
 txt_file = os.path.join(os.path.dirname(model), file_name)
 print(txt_file)
 with open(txt_file, 'w') as f_h:
+    print(eval_args)
     print(' '.join(eval_args))
     task = subprocess.Popen(eval_args, stdout=f_h)
     task.wait()
